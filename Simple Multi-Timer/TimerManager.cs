2b9b409e715f5e8c51d1929a30a6e5f19834dd22
Disaczar <Disaczar@gmail.com>
2023-10-11 22:26:20 +0700

    Added: TimerManager class

---
+++ Simple Multi-Timer/TimerManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Timers;
+
+namespace Simple_Multi_Timer
+{
+    internal class TimerManager
+    {
+        private List<TimerItem> timers = new List<TimerItem>();
+        private int updateInterval = 500;
+        private System.Timers.Timer timerUpdater;
+
+        public TimerManager()
+        {
+            timerUpdater = new System.Timers.Timer(updateInterval);
+            timerUpdater.Elapsed += UpdateAll;
+            timerUpdater.AutoReset = true;
+            timerUpdater.Enabled = true;
+        }
+
+        public void UpdateAll(object source, ElapsedEventArgs e)
+        {
+            foreach (TimerItem timer in timers)
+            {
+                timer.Update(updateInterval);
+            }
+        }
+
+        public void Start()
+        {
+            foreach (TimerItem timer in timers)
+            {
+                timer.Start();
+            }
+            timerUpdater.Start();
+        }
+
+        public void Stop()
+        {
+            foreach(TimerItem timer in timers)
+            {
+                timer.Stop();
+            }
+            timerUpdater.Stop();
+        }
+
+        public void Add(TimerItem item)
+        {
+            timers.Add(item);
+        }
+
+        public void Remove(int index)
+        {
+            timers.RemoveAt(index);
+        }
+    }
+}
