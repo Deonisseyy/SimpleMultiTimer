036894375e328ee3da467719d2dcb2b277683a80
Disaczar <Disaczar@gmail.com>
2023-10-11 20:20:12 +0700

    Initial commit

---
+++ Simple Multi-Timer/Program.cs
@@ -0,0 +1,12 @@
+namespace Simple_Multi_Timer
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+        }
+    }
+}
